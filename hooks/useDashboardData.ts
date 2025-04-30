cca73381ad12a6e4e47b79efb554d5002e17f958
Muhammad Ghazanfar Ali <muhammadghazanfarali@Muhammads-MacBook-Air.local>
2025-04-30 23:21:37 +0400

    fix data loss issue

---
+++ hooks/useDashboardData.ts
@@ -0,0 +1,28 @@
+import { dashboardApi } from "@/api";
+import { useEffect, useState } from "react";
+
+export function useDashboardData() {
+  const [dashboardData, setDashboardData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>(null);
+
+  const fetchDashboardData = () => {
+    setIsLoading(true);
+    dashboardApi
+      ?.getAccountantDashboard()
+      .then((res) => setDashboardData(res?.data))
+      .catch((err) => setError(err?.response.data.detail))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  return {
+    dashboardData,
+    isLoadingDashboardData: isLoading,
+    dashboardDataError: error,
+    refetchDashboardData: fetchDashboardData,
+  };
+}
